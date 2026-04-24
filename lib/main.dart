548b234f339dfe28b907bbc8ded9f3320985b267
SandyArdiyan <sanardiyan881@gmail.com>
2026-04-24 11:09:05 +0700

    Perbaiki pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'bloc/user_bloc.dart';
 import 'bloc/user_event.dart';
-import 'data/repository/user_repository_impl.dart';
+import 'data/repositories/user_repository_impl.dart';
 import 'helper/database_helper.dart';
 import 'pages/home_page.dart';
 
